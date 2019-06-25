ea1d6a7846ec898ebbca9aa6c85d3157b582589a
aokatz <andrey.katz@gmail.com>
2019-06-25 19:08:39 +0300

    3 0 fix

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -6,7 +6,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MealRepository {
-    Meal save(Meal meal);
+    // null if updated meal do not belong to userId
+    Meal save(Meal meal, int userId);
 
     // false if meal do not belong to userId
     boolean delete(int id, int userId);
